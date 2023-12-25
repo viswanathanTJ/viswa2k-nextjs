2b763ac3906595d23b2016cf110e524e667e5543
viswanathantj <viswanthan.tj@zohocorp.com>
2023-12-25 12:36:54 +0530

    added chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
+      <script async src="//code.tidio.co/ytyedcgnlikxcich6p1ypzpretyfymzr.js" />
     </html>
   );
 }
